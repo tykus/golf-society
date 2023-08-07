1f21190112a1eb3a0639c93a3c4b45a5e08b5097
Brian O'Sullivan <tykus@Brians-iMac.local>
2023-08-07 16:20:40 +0100

    Golf model, updates Users table migration and seeding

---
+++ database/seeders/GolferSeeder.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Golfer;
+
+class GolferSeeder extends Seeder
+{
+    protected string $model = Golfer::class;
+    protected string $table = 'golfers';
+}
