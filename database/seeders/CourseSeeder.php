707e6fa85c718759dfe20692f57aaafcbb866d69
Brian O'Sullivan <tykus@Brians-iMac.local>
2023-08-07 16:31:23 +0100

    Course model, migration and seeder

---
+++ database/seeders/CourseSeeder.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Database\Seeders;
+
+
+use App\Models\Course;
+
+class CourseSeeder extends Seeder
+{
+    public string $table = 'courses';
+    public string $model = Course::class;
+}
