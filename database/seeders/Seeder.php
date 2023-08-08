b5731524dfe7cd4fc870bbb1d7e1fb77df353f92
Brian O'Sullivan <tykus@Brians-iMac.local>
2023-08-08 21:34:07 +0100

    Model, migration and seeder for competitions

--- database/seeders/Seeder.php
+++ database/seeders/Seeder.php
@@ -6,21 +6,21 @@ use Illuminate\Database\Seeder as BaseSeeder;
 
 class Seeder extends BaseSeeder
 {
-    protected string $model;
-    protected string $table;
+    protected string $modelClass;
+    protected string $seedKey;
 
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        throw_unless(class_exists($this->model), new \Exception('No model class'));
-        throw_unless($this->model, new \Exception('No table key provided'));
+        throw_unless(class_exists($this->modelClass), new \Exception('No model class'));
+        throw_unless($this->modelClass, new \Exception('No seed key provided'));
 
         $seeds = json_decode(file_get_contents(__DIR__ . '/seeds.json'), true);
 
-        foreach($seeds[$this->table] ?? [] as $data) {
-            $this->model::factory()->create($data);
+        foreach($seeds[$this->seedKey] ?? [] as $data) {
+            $this->modelClass::factory()->create($data);
         }
     }
 }
