9432f4447186cccc179171056a52f880b785af07
Brian O'Sullivan <tykus@Brians-iMac.local>
2023-08-08 08:50:32 +0100

    Course model, migration and seeds

---
+++ database/factories/HoleFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Course;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Database\Eloquent\Factories\Sequence;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Hole>
+ */
+class HoleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'course_id' => Course::factory(),
+            'number' => fake()->randomElement(range(1, 18)),
+            'par' => fake()->randomElement([3, 4, 5]),
+            'stroke_index' => fake()->randomElement(range(1, 18)),
+        ];
+    }
+}
