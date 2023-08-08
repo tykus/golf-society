a84f5a5f8643400069657350ad9f49ad916b0102
Brian O'Sullivan <tykus@Brians-iMac.local>
2023-08-09 00:27:55 +0100

    Strokes taken

---
+++ database/seeders/ScoreSeeder.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Database\Seeders;
+
+
+use App\Models\Score;
+
+class ScoreSeeder extends Seeder
+{
+    protected string $modelClass = Score::class;
+    protected string $seedKey = 'scores';
+}
