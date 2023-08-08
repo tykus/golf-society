facaf55dc1c704f3e38011050d2a39759d7afc7c
Brian O'Sullivan <tykus@Brians-iMac.local>
2023-08-08 23:39:50 +0100

    Model &  migration for Scorecards

---
+++ database/seeders/ScorecardSeeder.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Database\Seeders;
+
+
+use App\Models\Scorecard;
+
+class ScorecardSeeder extends Seeder
+{
+    protected string $modelClass = Scorecard::class;
+    protected string $seedKey = 'scorecards';
+}
