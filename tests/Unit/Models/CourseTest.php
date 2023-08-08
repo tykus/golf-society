9e46842d6147753bce1bb5973c67e9f57c996714
Brian O'Sullivan <tykus@Brians-iMac.local>
2023-08-08 20:14:30 +0100

    Course test for `name`

--- tests/Unit/Models/CourseTest.php
+++ tests/Unit/Models/CourseTest.php
@@ -6,10 +6,14 @@ use Tests\TestCase;
 
 uses(TestCase::class, RefreshDatabase::class);
 
+it('has a name', function () {
+    $course = Course::factory()->create(['name' => 'Knightsbrook Golf Club']);
+    expect($course->name)->toBe('Knightsbrook Golf Club');
+});
+
 it('has a total par for the Course', function () {
     $course = Course::factory()
         ->hasHoles(18, ['par' => 4])
         ->create();
-
     expect($course->par)->toBe(72);
 });
